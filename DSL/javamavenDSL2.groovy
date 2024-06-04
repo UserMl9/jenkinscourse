4d5b9415e75264c7cc653f10e8ab49378ccf9306
UserMl9 <aprendizajeml9@gmail.com>
2024-06-04 14:55:01 -0300

    Create javamavenDSL2.groovy

---
+++ DSL/javamavenDSL2.groovy
@@ -0,0 +1,44 @@
+job('Java Maven App DSL 2') {
+    description('Java Maven App with DSL for Jenkins Course')
+    scm {
+        git('https://github.com/UserMl9/jenkinscourse/new/main', 'main') { node ->
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('aprendizajeml9@gmail.com')
+        }
+    }
+    steps {
+        maven {
+          mavenInstallation('mavenjenkins')
+          goals('-B -DskipTests clean package')
+        }
+        maven {
+          mavenInstallation('mavenjenkins')
+          goals('test')
+        }
+        shell('''
+          echo "Delivery: Deploying the application" 
+          java -jar "/var/jenkins_home/workspace/Java Maven App DSL/target/my-app-1.0-SNAPSHOT.jar"
+        ''')  
+    }
+    publishers {
+        archiveArtifacts('target/*.jar')
+        archiveJunit('target/surefire-reports/*.xml')
+	slackNotifier {
+            notifyAborted(true)
+            notifyEveryFailure(true)
+            notifyNotBuilt(false)
+            notifyUnstable(false)
+            notifyBackToNormal(true)
+            notifySuccess(true)
+            notifyRepeatedFailure(false)
+            startNotification(false)
+            includeTestSummary(false)
+            includeCustomMessage(false)
+            customMessage(null)
+            sendAs(null)
+            commitInfoChoice('NONE')
+            teamDomain(null)
+            authToken(null)
+       }
+    }
+}
