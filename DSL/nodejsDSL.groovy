8563bfadf22254860cb403d57b4fb4c68fd6905a
UserMl9 <aprendizajeml9@gmail.com>
2024-06-12 11:50:13 -0300

    Create nodejsDSL.groovy

---
+++ DSL/nodejsDSL.groovy
@@ -0,0 +1,37 @@
+job('Node.js App') {
+    description('Node js app for the jenkins course')
+    scm {
+        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('UserMl9')
+            node / gitConfigEmail('aprendizajeml9@gmail.com')
+        }
+    }
+    triggers {
+        scm('H/7 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs')
+    }
+    steps {
+        shell("npm install")
+    }
+    publishers {
+	slackNotifier {
+            notifyAborted(true)
+            notifyEveryFailure(true)
+            notifyNotBuilt(false)
+            notifyUnstable(false)
+            notifyBackToNormal(true)
+            notifySuccess(true)
+            notifyRepeatedFailure(false)
+            startNotification(false)
+            includeTestSummary(false)
+            includeCustomMessage(false)
+            customMessage(null)
+            sendAs(null)
+            commitInfoChoice('NONE')
+            teamDomain(null)
+            authToken(null)
+        }
+    }
+}
