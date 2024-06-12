596bc1dd06aa38a04658d893a5791bab43f77e96
UserMl9 <aprendizajeml9@gmail.com>
2024-06-11 21:34:14 -0300

    Create javamavenDSL3.groovy

---
+++ DSL/javamavenDSL3.groovy
@@ -0,0 +1,53 @@
+job('Java Maven App DSL3') {
+    description('Job of a Java application with Maven')
+    scm {
+        git('https://github.com/UserMl9/jenkinscourse/new/main', 'main') { node ->
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('aprendizajeml9@gmail.com')
+        }
+    }
+    steps {
+        maven {
+          mavenInstallation('mavenjenkins')
+          goals('-B -DskipTests clean package')
+        }
+        maven {
+          mavenInstallation('mavenjenkins')
+          goals('test')
+        }
+        shell('''
+          echo "Delivery: Deploying the application" 
+          java -jar "/var/jenkins_home/workspace/Java Maven with App DSL3/target/my-app-1.0-SNAPSHOT.jar"
+        ''')  
+    }
+    publishers {
+        archiveArtifacts('target/*.jar')
+        archiveJunit('target/surefire-reports/*.xml')
+	slackNotifier {
+            notifyAborted(true)
+            notifyEveryFailure(true)
+            notifyNotBuilt(false)
+            notifyUnstable(false)
+            notifyBackToNormal(true)
+            notifySuccess(true)
+            notifyRepeatedFailure(false)
+            startNotification(false)
+            includeTestSummary(false)
+            includeCustomMessage(false)
+            customMessage(null)
+            sendAs(null)
+            commitInfoChoice('NONE')
+            teamDomain(null)
+            authToken(null)
+       }
+    }
+}
+
+job('Test Job Hello World'){
+    description('Test Job Hello World')
+    steps {
+        shell('''
+            echo "Hello World!!" 
+        ''')  
+    }
+}
