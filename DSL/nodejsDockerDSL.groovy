03d89d844e62b71b6bfc7e2b285a7da81a35de49
UserMl9 <aprendizajeml9@gmail.com>
2024-06-23 17:53:18 -0300

    Create nodejsDockerDSL.groovy

---
+++ DSL/nodejsDockerDSL.groovy
@@ -0,0 +1,44 @@
+job('Node.js Docker App DSL') {
+    description('Node JS hello-world Docker Application for Jenkins')
+    scm {
+        git('https://github.com/UserMl9/jenkinscourse.git', 'main') { node ->
+            node / gitConfigName('UserMl9')
+            node / gitConfigEmail('aprendizajeml9@gmail.com')
+        }
+    }
+    triggers {
+        scm('H/7 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs')
+    }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('aprendizajeml9/nodejsapp')
+            tag('${GIT_REVISION,length=7}')
+            registryCredentials('docker-hub')
+            forcePull(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+    publishers {
+      slackNotifier {
+                notifyAborted(true)
+                notifyEveryFailure(true)
+                notifyNotBuilt(false)
+                notifyUnstable(false)
+                notifyBackToNormal(true)
+                notifySuccess(true)
+                notifyRepeatedFailure(false)
+                startNotification(false)
+                includeTestSummary(false)
+                includeCustomMessage(false)
+                customMessage(null)
+                sendAs(null)
+                commitInfoChoice('NONE')
+                teamDomain(null)
+                authToken(null)
+        }
+    }
+}
